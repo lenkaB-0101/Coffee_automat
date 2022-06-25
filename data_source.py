25c82bbe1f2f023e8c5479d12e594ffe76d14a5a
lenkaB-0101 <96007039+lenkaB-0101@users.noreply.github.com>
2022-06-25 19:10:24 +0200

    Add files via upload

---
+++ data_source.py
@@ -0,0 +1,34 @@
+MENU = {
+    "espresso": {
+      "ingredients": {
+        "water" : 50,
+        "milk" : 0,
+        "coffee": 20,
+      },
+      "cost": 40,
+    },
+    "latte": {
+      "ingredients": {
+        "water" : 200,
+        "milk" : 150,
+        "coffee": 25,
+      },
+      "cost": 60,
+    },
+     "cappuccino": {
+      "ingredients": {
+        "water" : 250,
+        "milk" : 100,
+        "coffee": 25,
+      },
+      "cost": 50,
+    }
+}
+
+resources = {
+    "water" : 500,
+    "milk" : 400,
+    "coffee": 200,
+}
+
+
